3db1dcf5614c4489cab06c3c00c8121926ecfbc5
stoneskill <244540499@qq.com>
2015-12-16 11:48:39 +0800

    0.0.2

--- src/app/main.js
+++ src/app/main.js
@@ -30,7 +30,7 @@ function updateUiState() {
 }
 
 function sendNativeMessage() {
-  message = {"text": document.getElementById('input-text').value};
+  message = document.getElementById('input-text').value;
   port.postMessage(message);
   appendMessage("Sent message: <b>" + JSON.stringify(message) + "</b>");
 }
